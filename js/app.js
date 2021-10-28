35067b7e637883224cd5991f979a15477ed12808
Vincent LAURENT <vince.laurent@gmail.com>
2021-10-28 14:48:50 +0200

    Navigation entre les questions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 var app = new Vue({
   el: '#app',
   data: {
-    questionnaire: questionnaire
+    questionnaire: questionnaire,
+    questionCourante: null
+  },
+  methods: {
+    questionSuivante: function (event) {
+      qKeyFind = (this.questionCourante === null);
+      for(qKey in this.questionnaire.questions) {
+        if(qKeyFind) {
+          this.questionCourante = qKey;
+          break;
+        }
+        if(qKey == this.questionCourante) {
+          qKeyFind = true;
+        }
+      }
+    },
+    questionPrecedente: function (event) {
+      qKeyPecedente = null;
+      for(qKey in this.questionnaire.questions) {
+        if(qKey == this.questionCourante) {
+          this.questionCourante = qKeyPecedente;
+          break;
+        }
+        qKeyPecedente = qKey;
+      }
+    }
   }
 });
