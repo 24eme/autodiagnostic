fa93e8e3bf183c5667aa175f6e9f9905edfebde0
Vincent LAURENT <vince.laurent@gmail.com>
2021-10-28 16:24:02 +0200

    Stockage de la question dans l'url

--- js/app.js
+++ js/app.js
@@ -2,17 +2,39 @@ var app = new Vue({
   el: '#app',
   data: {
     questionnaire: questionnaire,
-    questionCourante: null,
+    questionCouranteKey: null
+  },
+  created: function() {
+    window.addEventListener('hashchange',this.hashChange);
+    this.hashChange();
   },
   methods: {
+    hashChange: function() {
+      var url = new URL(window.location);
+      if(!url.hash.replace(/^#/, '')) {
+        return;
+      }
+      this.questionCouranteKey = url.hash.replace(/^#/, '');
+      this.loadQuestionCourante();
+    },
+    loadQuestionCourante: function() {
+      document.title = 'Autodiagnostic - ' + this.getQuestionCourante().libelle;
+      const url = new URL(window.location);
+      url.hash = this.questionCouranteKey;
+      history.pushState({}, this.getQuestionCourante().libelle, url)
+    },
     getQuestionCouranteIndex: function() {
       keys = Object.keys(this.questionnaire.questions);
 
-      return keys.indexOf(this.questionCourante);
+      return keys.indexOf(this.questionCouranteKey);
     },
     getNumeroQuestion: function() {
 
       return this.getQuestionCouranteIndex() + 1;
+    },
+    getQuestionCourante: function() {
+
+      return this.questionnaire.questions[this.questionCouranteKey];
     },
     getNbQuestions: function() {
 
@@ -26,12 +48,12 @@ var app = new Vue({
       if(this.isLastQuestion()) {
         return this.terminer();
       }
-
-      this.questionCourante = keys[this.getQuestionCouranteIndex() + 1];
+      this.questionCouranteKey = keys[this.getQuestionCouranteIndex() + 1];
+      this.loadQuestionCourante()
     },
     questionPrecedente: function (event) {
-
-      this.questionCourante = keys[this.getQuestionCouranteIndex() - 1];
+      this.questionCouranteKey = keys[this.getQuestionCouranteIndex() - 1];
+      this.loadQuestionCourante();
     },
     terminer: function (event) {
 
