e6531800351173e989432c16cdcab6fbc91cd0ce
Gabriel Poma <gpoma@24eme.fr>
2021-12-20 19:41:10 +0100

    ui: ajout bibliotheque charts

---
+++ public/js/charts.js
@@ -0,0 +1,28 @@
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+
+const config = {
+  type: 'radar',
+  data: data
+}
+
+const radarChart = new Chart(
+  document.getElementById('resultats-radar-chart'),
+  config
+)
