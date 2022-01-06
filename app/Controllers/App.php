28b431269b2ee6eab3f11a0b8ac90d442ab5fd3b
Gabriel Poma <gpoma@24eme.fr>
2022-01-06 13:39:18 +0100

    authentification cas pour réponse aux questions

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -3,12 +3,36 @@
 namespace Controllers;
 
 use Base;
+use phpCAS;
 use Statistiques;
 use Template;
 use Web;
 
 class App
 {
+    public function beforeroute(Base $f3)
+    {
+        require_once $f3->get('CAS_PATH').'/CAS.php';
+        phpCAS::setVerbose(false);
+        if ($f3->get('DEBUG')) {
+            phpCAS::setDebug();
+            phpCAS::setVerbose(true);
+        }
+
+        phpCAS::client(
+            CAS_VERSION_2_0,
+            $f3->get('CAS_HOST'),
+            $f3->get('CAS_PORT'),
+            $f3->get('CAS_CONTEXT')
+        );
+
+        if ($f3->get('DEBUG')) {
+            phpCAS::setNoCasServerValidation();
+        }
+
+        phpCAS::forceAuthentication();
+    }
+
     public function index(Base $f3)
     {
         $f3->set('inc', 'index.htm');
