c2fe00d6b70ea20b50959e33d4f0bb0a71dcd781
Gabriel Poma <gpoma@24eme.fr>
2022-04-28 12:24:50 +0200

    resultat: rm 2ème graph

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,5 +1,4 @@
 const canvas = document.getElementById('resultats-radar-chart');
-const lolli = document.getElementById('lollipop-graph');
 
 const radarChart = new Chart(
   canvas,
@@ -26,47 +25,3 @@ const radarChart = new Chart(
     }
 );
 
-const lollipop = new Chart(
-  lolli,
-  {
-    type: 'bar',
-    plugins: [ChartDataLabels],
-    options: {
-      indexAxis: 'y',
-      responsive: true
-    },
-    data: {
-      labels: canvas.getAttribute('data-labels').split(','),
-      datasets: [
-        {
-          label: 'AZERTY',
-          data: [
-            [0,3], [6,4], [3,9], [4,12], [2,10], [1,2]
-          ],
-          barThickness: 3,
-          backgroundColor: "#C29140",
-          datalabels: {
-            labels: {
-              moyenne: {
-                color: '#964320',
-                align: 'top',
-                anchor: 'start',
-                formatter: function (value, context) {
-                  return context.dataset.data[context.dataIndex][0]
-                }
-              },
-              perso: {
-                color: '#1C2436',
-                align: 'top',
-                anchor: 'end',
-                formatter: function (value, context) {
-                  return context.dataset.data[context.dataIndex][1]
-                }
-              }
-            }
-          }
-        }
-      ]
-    },
-  }
-);
