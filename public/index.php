73524318055e0af00373f60078687b3faa2c028d
Vincent LAURENT <vince.laurent@gmail.com>
2022-05-13 00:37:10 +0200

    Session de 24 heures

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('session.gc_maxlifetime', 86400);
 $f3 = require(__DIR__.'/../vendor/fatfree-core/base.php');
 $f3->config(__DIR__.'/../config/config.ini');
 $f3->config(__DIR__.'/../config/routes.ini');
